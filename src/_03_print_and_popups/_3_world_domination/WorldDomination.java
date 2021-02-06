3d98a1a03ddcd62422542470a7bf173b8b5a2f31
root <root@8cbdbd7d742b>
2021-02-06 02:31:51 +0000

    finished QuizGame, World Domination, and AwesomeorNot.

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,13 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+String answer = JOptionPane.showInputDialog(null,"Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if(answer.equalsIgnoreCase("yes")) {JOptionPane.showMessageDialog(null,"You Will Rule the World!");
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
-	}
+}
+if(answer.equalsIgnoreCase("no")) {JOptionPane.showMessageDialog(null,"Please go sign up for classes at login.jointheleague.org");
+}
+}
 }
 
--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,35 +2,38 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
-
+public static void main(String[] args) {
 	
 		Random ran = new Random();    //This will be used below to make a random number. 
 		
 		// 2. Make a variable that will hold a random whole number
-	
+	int random = ran.nextInt(4);
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
 	
 		// 3. Print your variable to the console
-	
+System.out.println(random);
 		// 4. Get the user to enter something that they think is awesome
-	
+String bruh = JOptionPane.showInputDialog("Enter Something Awesome!");
 		// 5. If your variable is  0
-	
+	if(random == 0) {JOptionPane.showMessageDialog(null,"That's super awesome!"); }
 			// -- tell the user whatever they entered is awesome!
 	
 		// 6. If your variable is  1
-	
+	if(random == 1) {JOptionPane.showMessageDialog(null, "That's cool I guess"); }
 			// -- tell the user whatever they entered is ok.
 	
 		// 7. If your variable is  2
 	
 			// -- tell the user whatever they entered is boring.
-	
+	if(random == 2) {JOptionPane.showMessageDialog(null,"Not gonna lie, that's pretty boring"); }
 		// 8. If your variable is  3
-	
+	if(random == 3) {JOptionPane.showMessageDialog(null,"How dare you say such a thing?! \nYou have wreaked havoc upon this Earth! \nAnd you will never be forgiven!"); }
 			// -- invent your own message to give to the user (be nice).
 
+}
 }
